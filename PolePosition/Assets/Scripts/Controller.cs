67530580eb1e5564c7fc1e3de80941fe53bf7129
speedsword <colingarvey.18@chagrinstudents.org>
2017-07-15 10:43:13 -0400

    I don't understand Unity

--- PolePosition/Assets/Scripts/Controller.cs
+++ PolePosition/Assets/Scripts/Controller.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class Controller : MonoBehaviour {
 
 	// Use this for initialization
+	public float movementForce;
+	public float topSpeed;
+	Rigidbody rb;
+
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			if (rb.velocity.magnitude < topSpeed) {
+				rb.AddForce (movementForce * transform.forward * Time.deltaTime);
+			}
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			if (rb.velocity.magnitude > 0) {
+				rb.AddForce (movementForce * -transform.forward * Time.deltaTime);
+			}
+		}
 	}
 }
