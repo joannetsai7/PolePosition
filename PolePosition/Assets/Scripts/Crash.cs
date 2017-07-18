537c9b3687daaf09132fcb37c1d698fb319eba2a
Joanne Tsai <joannets@andrew.cmu.edu>
2017-07-18 12:05:43 -0400

    new unity

---
+++ PolePosition/Assets/Scripts/Crash.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crash : MonoBehaviour {
+
+	public ParticleSystem carExplosion;
+	//public GameObject car;
+	private Controller control;
+	private Collider collide;
+	public GameObject camera;
+	// Use this for initialization
+	void Start () {
+		control = new Controller();
+		control = GetComponent<Controller>();
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		Instantiate(carExplosion, transform.position, transform.rotation);
+		control.movementForce = 0;
+		camera.transform.parent = null;
+		gameObject.SetActive(false);
+		collide = other;
+		Invoke("Respawn", 1f);
+	}
+	private void Respawn()
+	{
+		//Instantiate (car, new Vector3(250, 0, 250), transform.rotation);
+		camera.transform.parent = this.transform;
+		transform.position = collide.transform.position + collide.transform.forward*30;
+		transform.position = new Vector3(transform.position.x,-2,transform.position.z);
+		gameObject.SetActive (true);
+		camera.transform.rotation = transform.rotation;
+		camera.transform.Rotate (new Vector3 (15, 0, 0));
+		camera.transform.localPosition = /*transform.position + */new Vector3(0, 11, -20);
+	}
+}
