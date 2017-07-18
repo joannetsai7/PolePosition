cd63007179f7a2ddfc6e1a9f1382b9715c6e7e06
Joanne Tsai <joannets@andrew.cmu.edu>
2017-07-18 00:31:32 -0400

    more

---
+++ PolePosition/Assets/Scripts/CameraPan.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CameraPan : MonoBehaviour {
+
+	public Text insertCoin;
+	public Text pressSpace;
+	public AudioClip prepare;
+	public AudioClip theme;
+	public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource>();
+		source.PlayOneShot (theme, 0.9F);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up, Time.deltaTime * 20, Space.World);
+
+		if (Input.GetKey (KeyCode.Space)) {
+			StartCoroutine(Load ());
+		}
+	}
+
+	IEnumerator Load (){
+		source.Stop ();
+		source.PlayOneShot (prepare, 0.9F);
+		yield return new WaitForSeconds(1.5f);
+		SceneManager.LoadScene ("Scenes/TimeTrial");
+	}
+}
